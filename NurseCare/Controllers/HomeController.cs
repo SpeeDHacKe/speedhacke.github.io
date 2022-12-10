cf2f95135c8a775db7b7d36d98a4c25094e898b7
SpeeDHacKe <SpeeDHacKe@hotmail.com>
2022-12-11 02:25:31 +0700

    [ADD] : New Structure Code Project NurseCare

---
+++ NurseCare/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NurseCare.Models;
+using System.Diagnostics;
+using System.Net;
+
+namespace NurseCare.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3 | SecurityProtocolType.SystemDefault;
+            //var xx = ServicePointManager.SecurityProtocol;
+            //var tt = xx.CompareTo(SecurityProtocolType.Tls12);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
